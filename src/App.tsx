d7bc0b1e85b5f3fd6dd8115f24ccc75d5efeaba1
iliatech <iadomyshev@gmail.com>
2023-05-10 20:31:21 +0400

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./App.scss";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { DataTable } from "primereact/datatable";
@@ -8,33 +8,36 @@ import { getSecrets } from "./api/getSecrets";
 import { TypeTemplate } from "./components/columns/TypeTemplate";
 import { NameTemplate } from "./components/columns/NameTemplate";
 import { PassTemplate } from "./components/columns/PassTemplate";
-
-let isDataLoaded = false;
+import { ProgressSpinner } from "primereact/progressspinner";
 
 function App() {
   const [secrets, setSecrets] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (isDataLoaded) {
-      return;
-    }
+    setIsLoading(true);
 
     getSecrets(process.env.REACT_APP_DECRYPT_KEY ?? "").then((data: any) => {
       if (data?.items.length) {
         setSecrets(data.items);
       }
-    });
 
-    isDataLoaded = true;
-  });
+      setIsLoading(false);
+    });
+  }, []);
 
   return (
-    <div className="App">
+    <div className="app">
       <DataTable value={secrets} filterDisplay="row">
         <Column field="type" header="Type" body={TypeTemplate} filter />
         <Column field="name" header="Name" body={NameTemplate} filter />
-        <Column field="secret" header="Secret" body={PassTemplate} />
+        <Column field="secret" header="Pass" body={PassTemplate} />
       </DataTable>
+      {isLoading && (
+        <div className="app__spinner-container">
+          <ProgressSpinner />
+        </div>
+      )}
     </div>
   );
 }
