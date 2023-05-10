872ecbea8eeadc37e31e0c606a8fed3f88356084
iliatech <iadomyshev@gmail.com>
2023-05-10 22:13:51 +0400

    copy to clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.scss";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { getSecrets } from "./api/getSecrets";
@@ -9,10 +10,23 @@ import { TypeTemplate } from "./components/columns/TypeTemplate";
 import { NameTemplate } from "./components/columns/NameTemplate";
 import { PassTemplate } from "./components/columns/PassTemplate";
 import { ProgressSpinner } from "primereact/progressspinner";
+import { Toast } from "primereact/toast";
+import { capitalize } from "lodash";
 
 function App() {
   const [secrets, setSecrets] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const toast = useRef(null);
+
+  const showToast = (config: { type: string; text: string }) => {
+    if (toast.current) {
+      (toast.current as any)?.show({
+        severity: config.type,
+        summary: capitalize(config.type),
+        detail: config.text,
+      });
+    }
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -31,13 +45,16 @@ function App() {
       <DataTable value={secrets} filterDisplay="row">
         <Column field="type" header="Type" body={TypeTemplate} filter />
         <Column field="name" header="Name" body={NameTemplate} filter />
-        <Column field="secret" header="Pass" body={PassTemplate} />
+        <Column field="secret" header="Pass" body={PassTemplate}>
+          123
+        </Column>
       </DataTable>
       {isLoading && (
         <div className="app__spinner-container">
           <ProgressSpinner />
         </div>
       )}
+      <Toast ref={toast} />
     </div>
   );
 }
--- src/components/columns/PassTemplate.tsx
+++ src/components/columns/PassTemplate.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Secret } from "../../types/table";
 
-export const PassTemplate = () => {
-  return <div>*******</div>;
+const copyToClipboard = async (text: string): Promise<void> => {
+  try {
+    await navigator.clipboard.writeText(text);
+    //showToast({});
+    console.log("Content copied to clipboard");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
+
+export const PassTemplate = (item: Secret) => {
+  return (
+    <div>
+      *****
+      <i
+        className="pi pi-copy"
+        style={{ marginLeft: "10px", cursor: "pointer" }}
+        onClick={() => copyToClipboard(item.value)}
+      />
+    </div>
+  );
 };
