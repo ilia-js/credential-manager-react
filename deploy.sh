6fc2385881a7eccc311b93287c56f0695dfbb72b
iliatech <iadomyshev@gmail.com>
2023-07-18 22:18:12 +0400

    favicon

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 npm run build
-scp -r ~/Documents/HobbyProj/ID-credentials-manager/build/* iliatech@secret.iliatech.ru:/var/www/credentials.iliatech.ru/
+scp -r ./build/* iliatech@secret.iliatech.ru:/var/www/credentials.iliatech.ru/
