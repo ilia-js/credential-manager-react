ffe6e900750e1a5422bb2542aa9e91d581e083aa
iliatech <iadomyshev@gmail.com>
2023-10-18 10:45:06 +0200

    refactor

--- src/helpers/encryption.ts
+++ src/helpers/encryption.ts
@@ -1,13 +1,13 @@
-import cryptoJs from "crypto-js";
+import { AES, enc } from "crypto-js";
 
 export const aesEncrypt = (text: string, key: string): string => {
-  return cryptoJs.AES.encrypt(text, key).toString();
+  return AES.encrypt(text, key).toString();
 };
 
 export const aesDecrypt = (text: string, key: string): string => {
   try {
-    let bytes = cryptoJs.AES.decrypt(text, key);
-    return bytes.toString(cryptoJs.enc.Utf8);
+    let bytes = AES.decrypt(text, key);
+    return bytes.toString(enc.Utf8);
   } catch (e) {
     return "";
   }
